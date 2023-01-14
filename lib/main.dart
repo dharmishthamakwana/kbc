401680f9897a13c3b3bc385eec5b38e8b5b6bc26
Admin <makwanadharmishtha44@gmail.com>
2023-01-14 17:06:57 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:kbc/home.dart';
+import 'package:kbc/over.dart';
+import 'package:kbc/second.dart';
+import 'package:kbc/third.dart';
+
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/':(context) => Home(),
+        'Second':(context) => Second(),
+        'Third':(context) => Third(),
+        'Over':(context) => Over(),
+      },
+    ),
+  );
+}
